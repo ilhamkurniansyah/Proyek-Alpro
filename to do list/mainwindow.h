7dd32ba1ad39f019d8775f0ee7b70dae46e6f236
ilhamkurniansyah <ilhamkurniansyah@students.usu.ac.id>
2026-05-05 19:49:45 +0700

    Add files via upload

---
+++ to do list/mainwindow.h
@@ -0,0 +1,63 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFrame>
+#include <QDate>
+#include <QLabel>
+#include <QComboBox>
+#include <QListWidget>
+#include <QGridLayout>
+#include <QTimer>
+#include <QSet>
+
+struct TaskData
+{
+    QString title;
+    QString category;
+    QString priority;
+    QDate reminderDate;
+};
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
+
+private:
+    QDate currentDate;
+
+    QLabel *totalLabel;
+    QLabel *doneLabel;
+    QLabel *notDoneLabel;
+    QLabel *lateLabel;
+    QLabel *monthLabel;
+
+    QComboBox *categoryBox;
+    QComboBox *priorityBox;
+    QListWidget *taskList;
+    QGridLayout *calendarGrid;
+
+    QList<TaskData> tasks;
+
+    QTimer *notificationTimer;
+    QSet<QString> notifiedTasks;
+
+    QFrame* createStatCard(QLabel *&numberLabel, const QString &text, const QString &color);
+
+    void addTask();
+    void deleteTask();
+    void updateStats();
+    void buildCalendar();
+    void clearCalendar();
+    void addTaskToCalendarBox(QVBoxLayout *boxLayout, const QDate &date);
+    void checkNotifications();
+    void showTasksByDate(const QDate &date);
+};
+
+#endif
